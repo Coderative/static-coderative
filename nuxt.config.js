b94f926cf3547e3e266d956325d0695f0a133f1b
Vincent REHM <contact@vincentrehm.fr>
2021-03-25 17:48:10 +0100

    replacing and updating base generated assets and pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'static-coderative',
+    title: 'Codérative, le site officiel du collectif',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'fr',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Codérative - Nous sommes un collectif de développeurs, de designers et d’entrepreneurs expérimentés.' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">static-coderative</h1>
       <div class="links">
         <a
