7d5c1522324551ab9cc5bbbe39bad5f3fd7c8af3
Vincent REHM <contact@vincentrehm.fr>
2021-03-25 23:26:06 +0100

    update flex

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <section class="container h-screen w-full bg-yellow">
-    <div class="flex flex-col justify-between">
+    <div class="flex flex-col justify-between items-center">
       <h1 class="font-karla uppercase text-4xl md:text-9xl font-bold">
         Coderative
       </h1>
-      <div class="my-24 text-1xl md:text-6xl font-inconsolata">
+      <div class="w-2/3 my-24 text-1xl md:text-6xl font-inconsolata">
         <span>Nous sommes un collectif de développeurs, </span>
-        <br class="hidden md:visible" />
         <span>designers et d’entrepreneurs expérimentés.</span>
       </div>
     </div>
